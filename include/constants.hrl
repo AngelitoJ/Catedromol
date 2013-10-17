67feb76c227bfcb00bd54941abad606a59da335a
Angel J Alvarez Miguel <angel@uah.es>
2013-10-17 10:06:01 +0200

    Constansts
    Physical magnitudes need for the project

---
+++ include/constants.hrl
@@ -0,0 +1,19 @@
+
+
+
+-define(AU_TIME , 2.41888432e-2).% from femtoseconds to au
+-define(EH_EK , 3.157746e5).     % relation Eh/Kb Hartrees/Boltzmann
+-define(AU , 1822.88851543).     % relation N/e
+-define(A0 , 0.529177249).       % from bohr to angstrom
+-define(EH_KCAL , 627.50469,).   % from Hartrees to Kcal/mol
+-define(KB , 3.1668152037e-6 ).  % Boltzmann constant in au hartree/Kelvin
+
+
+
+     
+     
+     
+     
+     
+     
+  
